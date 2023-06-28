a55e68ec45339655b2227bd7b51d214ddf58d0ec
Dogacan <oobeya@testinium.com>
2023-06-28 21:28:15 +0300

    mongo added, cors support added

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from typing import List, Any
+
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from resources.route import api_router
 
 app = FastAPI()
@@ -13,6 +17,16 @@ async def root():
 async def root():
     return {"message": f"Hello world"}
 
-
-app = FastAPI()
 app.include_router(api_router)
+
+BACKEND_CORS_ORIGINS: List[Any] = [
+        "*"
+    ]
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
