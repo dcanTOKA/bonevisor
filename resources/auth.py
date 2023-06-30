63c846bf1a8b1ddde58b09cfe4c4622ff8efc369
Dogacan <oobeya@testinium.com>
2023-06-30 13:48:42 +0300

    extra slash characters removed

--- resources/auth.py
+++ resources/auth.py
@@ -6,14 +6,14 @@ from schemas.request import user as user_schema
 router = APIRouter(tags=["Auth"])
 
 
-@router.post("/register/", status_code=201)
+@router.post("/register", status_code=201)
 async def register(user_data: user_schema.UserRegisterIn):
     token = await UserManager.register(user_data.dict())
 
     return {'token': token}
 
 
-@router.post("/login/")
+@router.post("/login")
 async def login(user_data: user_schema.UserLoginIn):
     token = await UserManager.login(user_data.dict())
 
--- resources/user.py
+++ resources/user.py
@@ -10,7 +10,7 @@ from schemas.response.user import UserOut
 router = APIRouter(tags=["Users"])
 
 
-@router.get("/users/", dependencies=[Depends(oauth2_scheme)],
+@router.get("/users", dependencies=[Depends(oauth2_scheme)],
             response_model=List[UserOut])
 async def get_users(email: Optional[str] = None):
     if email:
